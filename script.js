ebb60fbd78d8014ac452c177335ee5e5d50aea83
alexpeteronoja <123165306+alexpeteronoja@users.noreply.github.com>
2025-01-10 10:10:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 let clickCount = 0;
 
 document.querySelector(".share-1-icon").addEventListener("click", function() {
-    clickCount++;
-
-    if (clickCount === 1) {
-        document.querySelector(".share-section").classList.remove("hide");
-    } else if (clickCount === 2) {
-        document.querySelector(".share-section").classList.add("hide");
-        clickCount = 0;
-    }
+   document.querySelector(".share-section").classList.toggle("hide");
 })
 
 document.querySelector(".share-2-icon").addEventListener("click", function() {
